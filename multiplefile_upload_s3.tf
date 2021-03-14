e5de5d5185e80bfc829fcb573f2b815049cb3643
chsiva <36084965+chsiva@users.noreply.github.com>
2021-03-14 14:26:53 -0400

    Update multiplefile_upload_s3.tf

--- multiplefile_upload_s3.tf
+++ multiplefile_upload_s3.tf
@@ -1,3 +1,17 @@
+provider "google" {
+  credentials = "${file("migrationaws2gcp-9e1f72b75761.json")}"
+  project = "migrationaws2gcp"
+  region = "us-central1"
+  zone = "us-central1-c"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name          = var.bucket_name
+  location      = "us-east1"
+  force_destroy = true
+}
+
+
 resource "google_storage_bucket_object" "multiple_upload" {
   for_each = fileset("upload/", "*")
   bucket = google_storage_bucket.my_bucket.id
